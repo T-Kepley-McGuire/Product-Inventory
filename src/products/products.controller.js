b967c3a341d5668e92dd5cf8106eb049d8519ec5
T-Kepley-McGuire <kepley.mcguire.mail@gmail.com>
2023-06-14 19:02:12 -0700

    Initial commit. More or less working, ready for deploying server

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,14 +1,45 @@
-function read(req, res, next) {
-  res.json({ data: { product_title: "some product title" } });
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const productService = require("./products.service");
+
+async function productExists(req, res, next) {
+  const product = await productService.read(req.params.productId);
+  if (product) {
+    res.locals.product = product;
+    return next();
+  }
+
+  next({ status: 404, message: `Product cannot be found.` });
 }
 
-function list(req, res, next) {
-  res.json({
-    data: [{ product_title: "product 1" }, { product_title: "product 2" }],
-  });
+function read(req, res) {
+  const { product: data } = res.locals;
+  res.json({ data });
+}
+
+async function list(req, res, next) {
+  const products = await productService.list();
+  res.json({ products });
+}
+
+async function listOutOfStockCount(req, res, next) {
+  const outOfStock = await productService.listOutOfStockCount();
+  res.json({ data: outOfStock });
+}
+
+async function listPriceSummary(req, res, next) {
+  const priceSummary = await productService.listPriceSummary();
+  res.json({ data: priceSummary });
+}
+
+async function listTotalWeightByProduct(req, res, next) {
+  const weightByProduct = await productService.listTotalWeightByProduct();
+  res.json({ data: weightByProduct });
 }
 
 module.exports = {
-  read: [read],
-  list: [list],
+  read: [asyncErrorBoundary(productExists), read],
+  list: asyncErrorBoundary(list),
+  listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
+  listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
 };
--- src/suppliers/suppliers.router.js
+++ src/suppliers/suppliers.router.js
@@ -2,12 +2,12 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./suppliers.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").post(controller.create).all(methodNotAllowed);
-
 router
   .route("/:supplierId")
   .put(controller.update)
   .delete(controller.delete)
   .all(methodNotAllowed);
+
+router.route("/").post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
